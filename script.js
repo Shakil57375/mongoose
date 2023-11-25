8ddd27b3f9b6c1177bd09689a90da5a514e360b7
Shakil57375 <shakil57375@gmail.com>
2023-11-25 11:44:21 +0600

    how to change the user value added

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-
+const User = require("./User");
 mongoose
   .connect("mongodb://127.0.0.1:27017/test")
   .then(() => {
@@ -8,3 +8,13 @@ mongoose
   .catch((error) => {
     console.error("Error connecting to MongoDB:", error);
   });
+
+async function run() {
+  const user = await User.create({ name: "Shakil", age: 21 });
+  //   change the user name
+  user.name = "rahim";
+  await user.save();
+  console.log(user);
+}
+
+run();
